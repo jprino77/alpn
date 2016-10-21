73d93f0820f8bce88adeb8abf61ddbb03d4cd8d8
julian <julian.prino@gmail.com>
2016-10-20 21:01:45 -0300

    estadisticas globales

--- alpn/alpn/urls.py
+++ alpn/alpn/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from apps.juego.views   import index, seriacion, ordenar, transformar, porColor, resultadoJuego
-from apps.estadistica.views import estadistica,movimiento_partida,error_partida
+from apps.estadistica.views import estadistica,movimiento_partida,estadisticaEdad,error_partida, movimiento_edad, movimiento_error
 
 urlpatterns = patterns('',
     # Examples:
@@ -17,6 +17,9 @@ urlpatterns = patterns('',
     url(r'resultadoJuego', resultadoJuego),
     url(r'estadistica', estadistica),
     url(r'movimiento_partida', movimiento_partida),
+    url(r'edad', estadisticaEdad),
     url(r'error_partida', error_partida),
+    url(r'movEdad', movimiento_edad),
+    url(r'errEdad', movimiento_error),
     
 )
